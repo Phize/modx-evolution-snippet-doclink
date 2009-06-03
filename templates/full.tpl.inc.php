bcf77d584fe5f0470f88c0a7f34e2b066d80c9f3
Phize <info@phize.net>
2009-06-03 07:23:12 +0000

    [(base_uri)]となっていた部分を[(base_url)]に修正
    git-svn-id: svn://localhost/MODx/assets/snippets/doclink/trunk@43 6ce02de6-cfb7-2e46-b63e-5161d7ce3690

--- templates/full.tpl.inc.php
+++ templates/full.tpl.inc.php
@@ -3,12 +3,12 @@
 
 $this->set('tplOuter', '@CODE:<div class="doclink doclink-full">[+inner+]</div>');
 $this->set('tplInner', '@CODE:<ul>[+first+][+prev+][+home+][+next+][+last+]</ul>' . "\n" . '<p>[+page.current+] of [+page.total+] pages.</p>');
-$this->set('tplHome', '@CODE:<li class="home enabled"><a href="[(base_uri)][+home.uri+]" title="[+home.title+]">Home</a></li>');
+$this->set('tplHome', '@CODE:<li class="home enabled"><a href="[(base_url)][+home.uri+]" title="[+home.title+]">Home</a></li>');
 $this->set('tplParent', '');
-$this->set('tplFirst', '@CODE:<li class="first enabled"><a href="[(base_uri)][+first.uri+]" title="[+first.title+]">&laquo;&nbsp;First</a></li>');
-$this->set('tplPrev', '@CODE:<li class="previous enabled"><a href="[(base_uri)][+prev.uri+]" title="[+prev.title+]">&lt;&nbsp;Previous</a></li>');
-$this->set('tplNext', '@CODE:<li class="next enabled"><a href="[(base_uri)][+next.uri+]" title="[+next.title+]">Next&nbsp;&gt;</a></li>');
-$this->set('tplLast', '@CODE:<li class="last enabled"><a href="[(base_uri)][+last.uri+]" title="[+last.title+]">Last&nbsp;&raquo;</a></li>');
+$this->set('tplFirst', '@CODE:<li class="first enabled"><a href="[(base_url)][+first.uri+]" title="[+first.title+]">&laquo;&nbsp;First</a></li>');
+$this->set('tplPrev', '@CODE:<li class="previous enabled"><a href="[(base_url)][+prev.uri+]" title="[+prev.title+]">&lt;&nbsp;Previous</a></li>');
+$this->set('tplNext', '@CODE:<li class="next enabled"><a href="[(base_url)][+next.uri+]" title="[+next.title+]">Next&nbsp;&gt;</a></li>');
+$this->set('tplLast', '@CODE:<li class="last enabled"><a href="[(base_url)][+last.uri+]" title="[+last.title+]">Last&nbsp;&raquo;</a></li>');
 $this->set('tplHomeOff', '@CODE:<li class="home disabled">Home</li>');
 $this->set('tplParentOff', '');
 $this->set('tplFirstOff', '@CODE:<li class="first disabled">&laquo;&nbsp;First</li>');
